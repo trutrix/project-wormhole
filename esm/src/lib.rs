f245f3016aa34d7f901a3a422bfb1d4b146166b3
Colton Chaffos <trutrix@gmail.com>
2025-04-16 08:53:24 -0600

    Added dev module for easier use in submodules

--- esm/src/lib.rs
+++ esm/src/lib.rs
@@ -1,3 +1,15 @@
 pub mod structs;
-
 pub mod esm;
+
+
+mod dev {
+    pub use nom_derive::{Parse, NomLE};
+    pub use nom::IResult;
+    pub use nom::number::complete::*;
+    pub use nom::multi::many0;
+    pub use nom_derive::nom;
+
+
+    pub use super::structs::fourcc::FourCC;
+
+}
