2c32e5cff7007e18bb2956050c955d44ccfed041
Colton Chaffos <trutrix@gmail.com>
2025-04-16 08:52:23 -0600

    Added custom Debug and Display

--- esm/src/structs/fourcc.rs
+++ esm/src/structs/fourcc.rs
@@ -1,4 +1,17 @@
+use crate::dev::*;
 
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash, NomLE)]
 pub struct FourCC(pub [u8;4]);
+
+
+impl std::fmt::Debug for FourCC {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} {}", self.0, String::from_utf8_lossy(&self.0))
+    }
+}
+
+impl std::fmt::Display for FourCC {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", String::from_utf8_lossy(&self.0))
+    }
+}
