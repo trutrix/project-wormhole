e083151a42b61bff173b7bbd7c794bed31e2ccd1
Colton Chaffos <trutrix@gmail.com>
2025-04-18 12:57:33 -0600

    Added elasped time printout

--- esm/src/tests.rs
+++ esm/src/tests.rs
@@ -6,7 +6,9 @@ const ESM_PATH: &str = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fallo
 fn test1() {
     use crate::esm::*;
     let (mut hbuf, mut dbuf) = ESM1::create_buffers();
+    let mut start = std::time::Instant::now();
     let mut esm = ESM1::new(ESM_PATH, &mut hbuf, &mut dbuf).unwrap();
+    println!("Time to load: {:?}", start.elapsed());
 
     println!("Header: {:?}", esm.header);
 
