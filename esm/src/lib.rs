40ba88f96cab1a44f973eb4505f46f85e7f828fc
Colton Chaffos <trutrix@gmail.com>
2025-04-18 10:29:01 -0600

    Changed some modules

--- esm/src/lib.rs
+++ esm/src/lib.rs
@@ -1,12 +1,13 @@
 pub mod structs;
 pub mod esm;
-
+mod tests;
 
 mod dev {
     pub use nom_derive::{Parse, NomLE};
     pub use nom::IResult;
     pub use nom::number::complete::*;
     pub use nom::multi::many0;
+    pub use nom::bytes::complete::take;
     pub use nom_derive::nom;
 
 
