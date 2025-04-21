9e9aa0bf9306dba9aee7718ffae8474d2ce5ec72
Colton Chaffos <trutrix@gmail.com>
2025-04-21 14:31:30 -0600

    Removed bytes values from debug display

--- esm/src/structs/fourcc.rs
+++ esm/src/structs/fourcc.rs
@@ -6,7 +6,7 @@ pub struct FourCC(pub [u8;4]);
 
 impl std::fmt::Debug for FourCC {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?} {}", self.0, String::from_utf8_lossy(&self.0))
+        write!(f, "{}", String::from_utf8_lossy(&self.0))
     }
 }
 
