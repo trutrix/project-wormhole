9359541369c1df16819b93ab0e46e6396ed64007
Colton Chaffos <trutrix@gmail.com>
2025-04-21 12:18:30 -0600

    Added vectors and colors to structs

---
+++ esm/src/structs/colors.rs
@@ -0,0 +1,7 @@
+use crate::dev::*;
+use crate::structs::vectors::*;
+
+
+pub type Color4 = Vec4<u8>;
+pub type Color3 = Vec3<u8>;
+pub type Color2 = Vec2<u8>;
--- esm/src/structs/mod.rs
+++ esm/src/structs/mod.rs
@@ -1,5 +1,7 @@
+pub mod colors;
 pub mod strings;
 pub mod fourcc;
 pub mod field;
 pub mod group;
 pub mod record;
+pub mod vectors;
