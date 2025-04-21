9beca5ae4f37677f4f2835d71a97b017724c043b
Colton Chaffos <trutrix@gmail.com>
2025-04-21 14:25:10 -0600

    Changed test to use RawESM

--- esm/src/tests.rs
+++ esm/src/tests.rs
@@ -1,13 +1,19 @@
 use crate::{dev::*};
-
+use std::io::Read;
 const ESM_PATH: &str = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Fallout 4\\Data\\Fallout4.esm";
 
 #[test]
 fn test1() {
     use crate::esm::*;
-    let (mut hbuf, mut dbuf) = ESM1::create_buffers();
+
+    let mut file = std::fs::File::open(ESM_PATH).unwrap();
+    let mut buf = Vec::new();
+    file.read_to_end(&mut buf).unwrap();
+
+    // let (mut hbuf, mut dbuf) = ESM1::create_buffers();
     let start = std::time::Instant::now();
-    let mut esm = ESM1::new(ESM_PATH, &mut hbuf, &mut dbuf).unwrap();
+    // let mut esm = ESM1::new(ESM_PATH, &mut hbuf, &mut dbuf).unwrap();
+    let (_,esm) = RawESM::parse(&buf).unwrap();
     println!("Time to load: {:?}", start.elapsed());
 
     println!("Header: {:#?}", esm.header);
