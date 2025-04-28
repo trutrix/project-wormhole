fcba6a11cd80f4f64780d57101e850739af815ee
tRuTrIx <trutrix@gmail.com>
2025-04-27 21:50:40 -0600

    Created Group<T> and TopGroup, added dividers WIP

--- esm/src/structs/group.rs
+++ esm/src/structs/group.rs
@@ -1,8 +1,13 @@
 use std::fmt::Debug;
 
-use crate::dev::*;
+use proc::collect_and_define_groups;
 
+use crate::records::all::*;
+use crate::dev::*;
 use super::record::VersionControl;
+
+
+// ====================================================================================================
 
 
 #[derive(Debug, NomLE)]
@@ -12,6 +17,9 @@ pub struct GroupHeader {
     pub label: GroupLabel, // 8 bytes, reversed process
     pub version_control: VersionControl // TODO: Unsure if records and groups share the same version information
 }
+
+
+// ====================================================================================================
 
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -36,45 +44,24 @@ impl Parse<&[u8]> for GroupLabel {
         let (i, label_type) = le_u32(i)?;
 
         match label_type {
-            0 => {
-                Ok((i, GroupLabel::Top(data)))
-            }
-            1 => {
-                Ok((i, GroupLabel::WorldChildren(u32::from_le_bytes(data.0))))
-            }
-            2 => {
-                Ok((i, GroupLabel::InteriorCellBlock(i32::from_le_bytes(data.0))))
-            }
-            3 => {
-                Ok((i, GroupLabel::InteriorCellSubBlock(i32::from_le_bytes(data.0))))
-            }
-            4 => {
-                Ok((i, GroupLabel::ExteriorCellBlock([i16::from_le_bytes(data.0[0..2].try_into().unwrap()), i16::from_le_bytes(data.0[2..4].try_into().unwrap())])))
-            }
-            5 => {
-                Ok((i, GroupLabel::ExteriorCellSubBlock([i16::from_le_bytes(data.0[0..2].try_into().unwrap()), i16::from_le_bytes(data.0[2..4].try_into().unwrap())])))
-            }
-            6 => {
-                Ok((i, GroupLabel::CellChildren(u32::from_le_bytes(data.0))))
-            }
-            7 => {
-                Ok((i, GroupLabel::TopicChildren(u32::from_le_bytes(data.0))))
-            }
-            8 => {
-                Ok((i, GroupLabel::CellPersistentChildren(u32::from_le_bytes(data.0))))
-            }
-            9 => {
-                Ok((i, GroupLabel::CellTemporaryChildren(u32::from_le_bytes(data.0))))
-            }
-            10 => {
-                Ok((i, GroupLabel::CellVisibleDistantChildren(u32::from_le_bytes(data.0))))
-            }
-            _ => {
-                Ok((i, GroupLabel::Unknown(data)))
-            }
+            0 => { Ok((i, GroupLabel::Top(data))) }
+            1 => { Ok((i, GroupLabel::WorldChildren(u32::from_le_bytes(data.0)))) }
+            2 => { Ok((i, GroupLabel::InteriorCellBlock(i32::from_le_bytes(data.0)))) }
+            3 => { Ok((i, GroupLabel::InteriorCellSubBlock(i32::from_le_bytes(data.0))))}
+            4 => { Ok((i, GroupLabel::ExteriorCellBlock([i16::from_le_bytes(data.0[0..2].try_into().unwrap()), i16::from_le_bytes(data.0[2..4].try_into().unwrap())]))) }
+            5 => { Ok((i, GroupLabel::ExteriorCellSubBlock([i16::from_le_bytes(data.0[0..2].try_into().unwrap()), i16::from_le_bytes(data.0[2..4].try_into().unwrap())]))) }
+            6 => { Ok((i, GroupLabel::CellChildren(u32::from_le_bytes(data.0)))) }
+            7 => { Ok((i, GroupLabel::TopicChildren(u32::from_le_bytes(data.0))))}
+            8 => { Ok((i, GroupLabel::CellPersistentChildren(u32::from_le_bytes(data.0)))) }
+            9 => { Ok((i, GroupLabel::CellTemporaryChildren(u32::from_le_bytes(data.0)))) }
+            10 => { Ok((i, GroupLabel::CellVisibleDistantChildren(u32::from_le_bytes(data.0))))}
+            _ => { Ok((i, GroupLabel::Unknown(data))) }
         }
     }
 }
+
+
+// ====================================================================================================
 
 
 pub fn alloc_group(i: &[u8]) -> IResult<&[u8], (GroupHeader, &[u8])> {
@@ -82,6 +69,10 @@ pub fn alloc_group(i: &[u8]) -> IResult<&[u8], (GroupHeader, &[u8])> {
     let (i, raw) = take(header.size as usize - 24)(i)?;
     Ok((i, (header, raw)))
 }
+
+
+// ====================================================================================================
+
 
 pub struct RawGroup<'esm> {
     pub header: GroupHeader,
@@ -91,7 +82,7 @@ pub struct RawGroup<'esm> {
 impl<'esm, 'nom> Parse<&'nom[u8]> for RawGroup<'esm> where 'nom: 'esm {
     fn parse(i: &'nom[u8]) -> IResult<&'nom[u8], Self, nom::error::Error<&'nom[u8]>> {
         let (i, (header, data)) = alloc_group(i)?;
-        let (i, records) = many0(complete(RawRecord::parse))(data)?;
+        let (_, records) = many0(complete(RawRecord::parse))(data)?;
         Ok((i, RawGroup { header, data: records }))
     }
 }
@@ -99,5 +90,188 @@ impl<'esm, 'nom> Parse<&'nom[u8]> for RawGroup<'esm> where 'nom: 'esm {
 impl Debug for RawGroup<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "RawGroup {{ header: {:?}, data: [{} bytes] }}", self.header, self.data.len())
+    }
+}
+
+
+// ====================================================================================================
+
+
+#[derive(Debug)]
+pub struct Group<T> {
+    pub header: GroupHeader,
+    pub data: Vec<T>
+}
+
+impl<T: for<'nom> Parse<&'nom[u8]>> Parse<&[u8]> for Group<T> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom::error::Error<&[u8]>> {
+        let (i, (header, data)) = alloc_group(i)?;
+        let (_, records) = many0(complete(T::parse))(data)?;
+        Ok((i, Group { header, data: records }))
+    }
+}
+
+
+// ====================================================================================================
+
+
+#[derive(Debug)]
+pub enum TopGroup {
+    GameSettings(Group<GameSetting>),
+    Keywords(Group<Keyword>),
+    LCRT(LCRT),
+    AACT(Group<Action>),
+    TRNS(TRNS),
+    CMPO(CMPO),
+    TXST(TXST),
+    GLOB(GLOB),
+    DMGT(DMGT),
+    CLAS(CLAS),
+    FACT(FACT),
+    HDPT(HDPT),
+    RACE(RACE),
+    SOUN(SOUN),
+    ASPC(ASPC),
+    MGEF(MGEF),
+    LTEX(LTEX),
+    ENCH(ENCH),
+    SPEL(SPEL),
+    ACTI(Group<Activator>),
+    TACT(TACT),
+    ARMO(ARMO),
+    BOOK(BOOK),
+    CONT(CONT),
+    DOOR(DOOR),
+    INGR(INGR),
+    LIGH(LIGH),
+    MISC(MISC),
+    STAT(STAT),
+    SCOL(SCOL),
+    MSTT(MSTT),
+    GRAS(GRAS),
+    TREE(TREE),
+    FLOR(FLOR),
+    FURN(FURN),
+    WEAP(WEAP),
+    AMMO(Group<Ammo>),
+    NPC_(NPC_),
+    LVLN(LVLN),
+    KEYM(KEYM),
+    ALCH(Group<Alchemy>),
+    IDLM(IDLM),
+    NOTE(NOTE),
+    PROJ(PROJ),
+    HAZD(HAZD),
+    BNDS(BNDS),
+    TERM(TERM),
+    LVLI(LVLI),
+    WTHR(WTHR),
+    CLMT(CLMT),
+    SPGD(SPGD),
+    RFCT(RFCT),
+    REGN(REGN),
+    NAVI(NAVI),
+    CELL(Group<Cell>),
+    WRLD(Group<Worldspace>),
+    QUST(QUST),
+    IDLE(IDLE),
+    PACK(PACK),
+    CSTY(CSTY),
+    LSCR(LSCR),
+    ANIO(Group<AnimatedObject>),
+    WATR(WATR),
+    EFSH(EFSH),
+    EXPL(EXPL),
+    DEBR(DEBR),
+    IMGS(IMGS),
+    IMAD(IMAD),
+    FLST(FLST),
+    PERK(PERK),
+    BPTD(BPTD),
+    ADDN(Group<Addon>),
+    AVIF(AVIF),
+    CAMS(CAMS),
+    CPTH(CPTH),
+    VTYP(VTYP),
+    MATT(MATT),
+    IPCT(IPCT),
+    IPDS(IPDS),
+    ARMA(ARMA),
+    ECZN(ECZN),
+    LCTN(LCTN),
+    MESG(MESG),
+    DOBJ(DOBJ),
+    DFOB(DFOB),
+    LGTM(LGTM),
+    MUSC(MUSC),
+    FSTP(FSTP),
+    FSTS(FSTS),
+    SMBN(SMBN),
+    SMQN(SMQN),
+    SMEN(SMEN),
+    MUST(MUST),
+    DLVW(DLVW),
+    EQUP(EQUP),
+    RELA(RELA),
+    ASTP(ASTP),
+    OTFT(OTFT),
+    ARTO(ARTO),
+    MATO(MATO),
+    MOVT(MOVT),
+    SNDR(SNDR),
+    SNCT(SNCT),
+    SOPM(SOPM),
+    COLL(COLL),
+    CLFM(CLFM),
+    REVB(REVB),
+    PKIN(PKIN),
+    RFGP(RFGP),
+    AMDL(AMDL),
+    LAYR(LAYR),
+    COBJ(COBJ),
+    OMOD(OMOD),
+    MSWP(MSWP),
+    ZOOM(ZOOM),
+    INNR(INNR),
+    KSSM(KSSM),
+    AECH(AECH),
+    SCCO(SCCO),
+    AORU(AORU),
+    SCSN(SCSN),
+    STAG(STAG),
+    NOCM(NOCM),
+    LENS(LENS),
+    GDRY(GDRY),
+    OVIS(OVIS),
+    Unhandled    
+}
+
+impl Parse<&[u8]> for TopGroup {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom::error::Error<&[u8]>> {
+        let (i, (header, data)) = alloc_group(i)?;
+
+        match header.label {
+            GroupLabel::Top(label) => {
+                match &label.0 {
+                    b"GMST" => {
+                        let (_, records) = many0(complete(GameSetting::parse))(data)?;
+                        Ok((i, TopGroup::GameSettings(Group { header, data: records })))
+                    }
+                    b"KYWD" => {
+                        let (_, records) = many0(complete(Keyword::parse))(data)?;
+                        Ok((i, TopGroup::Keywords(Group { header, data: records })))
+                    }
+                    _ => {
+                        unimplemented!("Top group {} not implemented", label);
+                    }
+                }
+            }
+            _ => {
+                return Err(nom::Err::Error(nom::error::Error::new(i, nom::error::ErrorKind::Tag)));
+            }
+        }
+
+
+
     }
 }
