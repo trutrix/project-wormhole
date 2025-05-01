a8d963d25d1146602d3b44b26018d534ffed310f
tRuTrIx <trutrix@gmail.com>
2025-05-01 09:28:49 -0600

    Added SizedStrings

--- esm/src/structs/strings.rs
+++ esm/src/structs/strings.rs
@@ -1,10 +1,11 @@
 use crate::dev::*;
 
 
-
+// ====================================================================================================
 
 // Basically a CString
 // Custom implementation to avoid foreign parsing errors
+// TODO: Make this a proper CString
 #[derive(Debug)]
 pub struct ESMString(pub String);
 
@@ -13,5 +14,54 @@ impl Parse<&[u8]> for ESMString {
         let (_, left) = take(i.len()-1)(i)?;
         let s = String::from_utf8_lossy(left).to_string();
         Ok((&[], Self(s)))
+    }
+}
+
+impl std::fmt::Display for ESMString {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0) 
+    }
+}
+
+
+// ====================================================================================================
+
+#[derive(Debug)]
+pub struct SizedString8(pub String);
+
+impl Parse<&[u8]> for SizedString8 {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
+        let (i, size) = le_u8(i)?;
+        let (i, raw) = take(size)(i)?;
+        let s = String::from_utf8_lossy(raw).to_string();
+        Ok((i, Self(s)))
+    }
+}
+
+// ====================================================================================================
+
+#[derive(Debug)]
+pub struct SizedString16(pub String);
+
+impl Parse<&[u8]> for SizedString16 {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
+        let (i, size) = le_u16(i)?;
+        let (i, raw) = take(size)(i)?;
+        let s = String::from_utf8_lossy(raw).to_string();
+        Ok((i, Self(s)))
+    }
+}
+
+// ====================================================================================================
+
+#[derive(Debug)]
+pub struct SizedString32(pub String);
+
+impl Parse<&[u8]> for SizedString32 {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self> {
+        let (i, size) = le_u32(i)?;
+        let (i, raw) = take(size)(i)?;
+        let s = String::from_utf8_lossy(raw).to_string();
+        Ok((i, Self(s)))
     }
 }
