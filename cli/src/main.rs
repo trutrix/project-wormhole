9050ad32c09b78ee3195426fa8978317a721a4a1
Colton Chaffos <trutrix@gmail.com>
2025-09-29 08:39:10 -0600

    Added some boilerplate code

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to the ESM file
+    #[arg(short, long)]
+    path: String,
+
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let path = std::path::Path::new(&args.path);
+    if !path.exists() {
+        eprintln!("File does not exist: {}", args.path);
+        std::process::exit(1);
+    } else {
+        println!("Loading ESM file: {}", args.path);
+    }
 }
