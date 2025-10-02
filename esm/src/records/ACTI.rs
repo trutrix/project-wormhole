6109c3237ffa32af6a0a468f2b57604ee634c063
Colton Chaffos <trutrix@gmail.com>
2025-10-02 10:38:34 -0600

    Added field templates

--- esm/src/records/ACTI.rs
+++ esm/src/records/ACTI.rs
@@ -3,7 +3,48 @@ use crate::{dev::*, structs::geometry::ObjectBounds};
 define_record! {
     b"ACTI",
     Activator, [
+
+        b"ATTX", ActivateTextOverride, ESMString; // TODO: Verify
+        b"CIS1", Condition1, u8; // TODO: Find type
+        b"CIS2", Condition2, u8; // TODO: Find type
+        b"CITC", ConditionCount, u8; // TODO: Find type
+        b"CTDA", UnknownConditionData, u8; // TODO: Find type
+
+        // Compound destruction fields
+        b"DAMC", DamageResistance, u8; // TODO: Find type
+        b"DEST", Destructable, u8; // TODO: Find type
+        b"DMDL", DamageModel, FormId; // TODO: Verify
+        b"DMDT", DamageModelData, u8; // TODO: Find type
+        b"DSTA", DestructionName, u8; // TODO: Verify
+        b"DSTD", DestructionStageData, u8; // TODO: Find type
+        b"DSTF", DestructionFlags, u8; // TODO: Find type
+
+
         b"EDID", EditorId, ESMString;
+        b"FNAM", Flags, u8; // TODO: Find type
+        b"FTYP", ForcedLocRefType, u8; // TODO: Find type
+        b"FULL", Name, u32; // TODO: Find type
+
+        // Keywords
+        b"KSIZ", KeywordCount, u32; // TODO: Find type
+        b"KWDA", Keywords, u8; // TODO: Find type
+
+        // Model data
+        b"MODL", ModelPath, ESMString;
+        b"MODS", MaterialSwap, u8; // TODO: Find type
+        b"MODT", ModelTextureData, u8; // TODO: Find type
+
+
+        b"NTRM", NativeTerminal, u8; // TODO: Find type
         b"OBND", ObjectBounds, ObjectBounds;
+        b"PNAM", MarkerColor, u8; // TODO: Find type
+        b"PRPS", Properties, u8; // TODO: Find type
+        b"PTRN", PreviewTransform, u8; // TODO: Find type
+        b"RADR", RadioReciever, u8;
+        b"SNAM", SoundLooping, FormId; // TODO: Verify
+        b"STCP", AnimationSound, u8; // TODO: Find type
+        b"VMAD", VirtualMachineAdapter, u8; // TODO: Find type
+        b"VNAM", SoundActivation, u8; // TODO: Find type
+        b"WNAM", WaterType, u8; // TODO: Find type
     ]
 }
