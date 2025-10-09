d1a9083807307736566dd021245ac823803f4811
Colton Chaffos <trutrix@gmail.com>
2025-10-09 11:18:55 -0600

    Updated AnimatedObject Fields

--- esm/src/records/ANIO.rs
+++ esm/src/records/ANIO.rs
@@ -3,10 +3,9 @@ use crate::dev::*;
 define_record! {
     b"ANIO",
     AnimatedObject, [
-        b"EDID", EditorId, ESMString;
-        b"MODL", ModelMesh, ESMString;
-        b"MODT", ModelTexture, ESMString;
-        
+        EditorId;
+        AllModelData;
+        b"BNAM", UnloadEvent, ESMString; // String?
     ]
 }
 
