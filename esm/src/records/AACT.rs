a2b5bedd17596c6d5f21985d7378b4481ed71d37
Colton Chaffos <trutrix@gmail.com>
2025-10-09 08:27:58 -0600

    Updated Action with missing fields

--- esm/src/records/AACT.rs
+++ esm/src/records/AACT.rs
@@ -5,7 +5,9 @@ define_record! {
     Action, [
         EditorId;
         b"CNAM", Color, Color4;
-        b"TNAM", Type, u32;
+        b"TNAM", Type, u32; // TODO: Find correct type
         b"DNAM", Notes, ESMString;
+        b"FULL", FullName, u8; // TODO: Find correct type
+        b"DATA", AttractionRule, u8; // TODO: Find correct type
     ]
 }
