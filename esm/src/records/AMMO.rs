391d99a43a34b61a1886a4f94adeea188d9f7775
Colton Chaffos <trutrix@gmail.com>
2025-10-09 08:45:17 -0600

    Added more fields to AMMO

--- esm/src/records/AMMO.rs
+++ esm/src/records/AMMO.rs
@@ -3,22 +3,27 @@ use crate::{dev::*, structs::geometry::ObjectBounds};
 define_record! {
     b"AMMO",
     Ammo, [
-        b"EDID", EditorId, ESMString;
-        b"OBND", ObjectBounds, ObjectBounds;
+        EditorId;
+        ObjectBounds;
+        AllModelData;
 
-        b"PTRN", PreviewTransform, FormId;
+        b"PTRN", PreviewTransform, FormId; // Ref to TRNS
         b"FULL", FullName, ESMString; // LocalizedString?
         b"DESC", Description, ESMString; // LocalizedString?
-        
 
-        b"MODL", ModelPath, ModelPath;
-        b"MODT", ModelTexture, ModelTexture;
-        b"MODC", ModelColorMap, ModelColorMap;
-        b"MODS", ModelMaterialSwap, ModelMaterialSwap;
-        b"MODF", ModelFlags, ModelFlags;
+        // Unsure if KSIZ defines the actual size of KWDA
+        // Why wouldnt you be able to just divide the size of KWDA by 4?
+        // Similar to XXXX field in cells?
+        b"KSIZ", KeywordSize, u8; // Number of keywords
+        b"KWDA", Keywords, Vec<FormId>; // Refs to KYWDs
 
-
-
+        b"DATA", Data, (u32, f32); // Unknown struct
+        b"DNAM", Damage, u8; // TODO create struct for this - refs a projectile or something
+        b"ONAM", ShortName, ESMString; // LocalizedString?
+        b"NAM1", CasingModel, FormId; // Ref?
+        b"NAM2", ModelInfo, u8; // Unknown
+        b"ZNAM", SoundPutDown, FormId; // Ref to SNDR
+        b"YNAM", SoundPickUp, FormId; // Ref to SNDR
     ]
 }
 
