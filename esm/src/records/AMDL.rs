d949f4714d60b6fcb497052714ed534f6823a298
Colton Chaffos <trutrix@gmail.com>
2025-10-09 08:36:10 -0600

    Added note to AimModel

--- esm/src/records/AMDL.rs
+++ esm/src/records/AMDL.rs
@@ -1,13 +1,17 @@
 use crate::dev::*;
 
+// This record forgoes the usual fields in favor of a single DATA structure
+
 define_record! {
     b"AMDL",
     AimModel, [
-        b"EDID", EditorId, ESMString;
+        EditorId;
         b"DATA", AimModelData, AimModelData;
     ]
 }
 
+
+// TODO: Find actual structure
 #[derive(Debug, NomLE)]
 pub struct AimModelData {
     
