75285294ecd38b57d915bce87e01193463a94d92
Colton Chaffos <trutrix@gmail.com>
2025-10-09 08:34:26 -0600

    Added note for AudioEffectChain

--- esm/src/records/AECH.rs
+++ esm/src/records/AECH.rs
@@ -1,9 +1,13 @@
 use crate::dev::*;
 
+
+// This record reuses fields with the same names and puts them in an array
+// May require special handling to keep things in order
+
 define_record! {
     b"AECH",
     AudioEffectChain, [
-        b"EDID", EditorId, ESMString;
+        EditorId;
         b"KNAM", Keyword, u32;
         b"DNAM", DNAM, ESMString;
     ]
