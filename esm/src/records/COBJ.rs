e0e76ef7493d7112dbf7543d0166fb83329d6bd2
Colton Chaffos <trutrix@gmail.com>
2025-10-18 04:39:48 -0600

    Finished adding fields

--- esm/src/records/COBJ.rs
+++ esm/src/records/COBJ.rs
@@ -12,5 +12,10 @@ define_record! {
         b"BNAM", WorkbenchKeyword, FormId;
         b"FVPA", Components, Vec<(FormId, u32)>;
         b"ANAM", MenuArtObject, FormId;
+        b"INTV", CreatedObjectCount, u16; // Sometimes 4 bytes
+        b"NAM2", Nam2, FormId; // Always 4 bytes - Assuming FormId
+        b"NAM1", Nam1, FormId; // Always 4 bytes - Assuming FormId
+        b"FNAM", Categories, Vec<FormId>;
+        
     ]
 }
