97a0e70641a1f07261b546865bccc56b7045e8eb
Colton Chaffos <trutrix@gmail.com>
2025-10-22 10:44:07 -0600

    Changed more fields to common

--- esm/src/records/ACTI.rs
+++ esm/src/records/ACTI.rs
@@ -9,28 +9,26 @@ define_record! {
         ObjectBounds;
         PreviewTransform;
         Condition;
+        VirtualMachineAdapter;
+        Destructible;
+        FullName;
+        Keywords;
 
         b"ATTX", ActivateTextOverride, ESMString; // TODO: Verify
         b"CITC", ConditionCount, u8; // TODO: Find type
 
         // Compound destruction fields
         b"DAMC", DamageResistance, u8; // TODO: Find type
-        b"DEST", Destructable, u8; // TODO: Find type
-        b"DMDL", DamageModel, FormId; // TODO: Verify
-        b"DMDT", DamageModelData, u8; // TODO: Find type
+
+
+        
+        
         b"DSTA", DestructionName, u8; // TODO: Verify
-        b"DSTD", DestructionStageData, u8; // TODO: Find type
-        b"DSTF", DestructionFlags, u8; // TODO: Find type
 
 
         
         b"FNAM", Flags, u8; // TODO: Find type
         b"FTYP", ForcedLocRefType, u8; // TODO: Find type
-        FullName;
-
-        // Keywords
-        b"KSIZ", KeywordCount, u32; // TODO: Find type
-        b"KWDA", Keywords, u8; // TODO: Find type
         
 
         b"NTRM", NativeTerminal, u8; // TODO: Find type
@@ -41,7 +39,7 @@ define_record! {
         b"RADR", RadioReciever, u8;
         b"SNAM", SoundLooping, FormId; // TODO: Verify
         b"STCP", AnimationSound, u8; // TODO: Find type
-        VirtualMachineAdapter;
+        
         b"VNAM", SoundActivation, u8; // TODO: Find type
         b"WNAM", WaterType, u8; // TODO: Find type
     ]
