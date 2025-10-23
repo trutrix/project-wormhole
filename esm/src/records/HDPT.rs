b53e477b704941895fa9c46171b0d35af83837e2
Colton Chaffos <trutrix@gmail.com>
2025-10-23 10:12:17 -0600

    Added HDPT fields

--- esm/src/records/HDPT.rs
+++ esm/src/records/HDPT.rs
@@ -2,6 +2,16 @@ use crate::dev::*;
 
 define_record! {
     b"HDPT",
-    HDPT, [
+    HeadPart, [
+        EditorId;
+        FullName;
+        AllModelData;
+        Condition;
+        b"TNAM", TextureSet, FormId;
+        b"RNAM", ValidRaces, FormId;
+        b"DATA", Flags, u8;
+        b"PNAM", Type, u32;
+        b"NAM0", PartListType, u32;
+        b"NAM1", PartlistPath, ESMString;
     ]
 }
