6710956264ff9f9a12431a57cfb94de539f2b0b2
Colton Chaffos <trutrix@gmail.com>
2025-10-27 10:28:39 -0600

    Added missing liist from new flag test

--- esm/src/records/NPC_.rs
+++ esm/src/records/NPC_.rs
@@ -10,3 +10,20 @@ define_record! {
     NonPlayerCharacter, [
     ]
 }
+
+/* {NPC_: {DOFT, QNAM, TEND, CS2F, FMIN, 
+           EDID, RNAM, AIDT, KSIZ, LTPT, 
+           FMRI, SHRT, KWDA, NAM5, DPLT, 
+           STCP, CSCR, PRPS, CNAM, DSTD, 
+           RCLR, MSDK, DNAM, VMAD, CS2D, 
+           MWGT, TPLT, NAM8, HCLF, CS2E, 
+           ZNAM, NAM4, FMRS, OBTS, MSDV, 
+           FULL, TETI, NAM6, PFRN, DSTF, 
+           ECOR, CNTO, PRKR, PRKZ, DATA, 
+           APPR, VTCK, CS2H, PTRN, CRIF, 
+           PNAM, ATKR, FTST, OBTE, OBTF, 
+           SNAM, COCT, OBND, MRSV, WNAM, 
+           PKID, SOFT, LTPC, CS2K, FTYP, 
+           ACBS, GWOR, NTRM, INAM, DEST, 
+           SPCT, ANAM, TPTA, SPLO, STOP
+}}*/
