aae06d15db417c044178303b6c148952e922910c
tRuTrIx <trutrix@gmail.com>
2025-10-31 12:38:28 -0600

    Cleaned up warnings

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(unused)]
 use std::io::prelude::*;
 use esm::esm::RawESM;
 use nom_derive::*;
--- nif/src/lib.rs
+++ nif/src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 pub mod nif_header;
 pub mod nif_file;
 pub mod nif_block;
