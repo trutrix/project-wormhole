e618a1d78646cb7cc8db68ffcc8984a8a465a75c
tRuTrIx <trutrix@gmail.com>
2025-11-12 08:37:15 -0700

    Enhance FormId formatting to handle null values in Debug and Display implementations

--- esm/src/structs/form_id.rs
+++ esm/src/structs/form_id.rs
@@ -10,16 +10,31 @@ pub struct FormId(pub u32);
 
 impl std::fmt::Debug for FormId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "FormId({:08X})({})", self.0, self.0)
+        if self.is_null() {
+            write!(f, "FormId(NULL)")
+        } else {
+            write!(f, "FormId({:08X})({})", self.0, self.0)
+        }
+        
     }
 }
 
 
 impl std::fmt::Display for FormId {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:08X}", self.0)
+        if self.is_null() {
+            write!(f, "FormId(NULL)")
+        } else {
+            write!(f, "{:08X}", self.0)
+        }
     }
 }
 
 
 // ================================================================================
+
+impl FormId {
+    pub fn is_null(&self) -> bool {
+        self.0 == 0
+    }
+}
