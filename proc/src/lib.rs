45503a27ab8176d49006d71dc6df106fb6caa910
tRuTrIx <trutrix@gmail.com>
2025-11-18 13:04:46 -0700

    Added code example to define_records2

--- proc/src/lib.rs
+++ proc/src/lib.rs
@@ -11,6 +11,26 @@ pub fn define_record(input: TokenStream) -> TokenStream {
     out.into()
 }
 
+
+/// ```rust
+/// use proc::define_record2;
+/// 
+/// define_record2! {
+///     b"TEST", // FourCC identifier
+///     TestRecord, // Record Struct name
+///     [
+///         EditorId; // Common field
+///         b"CUST", CustomField, u32; // Custom field
+///         b"YNAM", PickUpSound, [b"SOUN"]; // Record reference and restraint to SOUN type
+///     ],
+///     // Flags - Position / Name
+///     [
+///         0x00000001, IsImportant;
+///     ]
+/// }
+/// 
+/// 
+/// ```
 #[proc_macro]
 pub fn define_record2(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as esm::RecordDefinition2);
