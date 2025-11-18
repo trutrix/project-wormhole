ce20d54b9e317264ffd0976500a5908fa4d640cb
tRuTrIx <trutrix@gmail.com>
2025-11-18 10:30:27 -0700

    Added KeywordType enum

--- esm/src/records/KYWD.rs
+++ esm/src/records/KYWD.rs
@@ -6,11 +6,37 @@ use crate::{dev::*, structs::colors::Color4};
 define_record! {
     b"KYWD", Keyword, [
         EditorId;
-        b"CNAM", Color, Color4;
         FullName;
+        b"CNAM", Color, Color4;
         b"DNAM", Notes, ESMString;
-        b"TNAM", Type, u32;
+        b"TNAM", Type, KeywordType;
         b"DATA", AttractionRule, u32;
         b"NNAM", DisplayName, ESMString;
     ]
+}
+
+
+#[derive(Debug, NomLE, Default)]
+#[repr(u32)]
+pub enum KeywordType {
+    #[default]
+    None = 0,
+    ComponentTechLevel = 1,
+    AttachPoint = 2,
+    ComponentProperty = 3,
+    InstantiationFilter = 4,
+    ModAssociation = 5,
+    Sound = 6,
+    AnimArchetype = 7,
+    FunctionCall = 8,
+    RecipeFilter = 9,
+    AttractionType = 10,
+    DialogueSubtype = 11,
+    QuestTarget = 12,
+    AnimFlavor = 13,
+    AnimGender = 14,
+    AnimFace = 15,
+    QuestGroup = 16,
+    AnimInjured = 17,
+    DispelEffect = 18,
 }
