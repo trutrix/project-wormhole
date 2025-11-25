cea9b2ff6059ad86b7c139a78fcde119c4090388
tRuTrIx <trutrix@gmail.com>
2025-11-25 08:29:23 -0700

    Just comments, tried a new system but decided against it

--- esm/src/structs/world_entry.rs
+++ esm/src/structs/world_entry.rs
@@ -2,7 +2,6 @@ use crate::{dev::*, records::all::{Cell, Worldspace}};
 
 
 #[derive(Debug, NomLE)]
-#[repr(C)]
 pub struct WorldEntry {
     pub worldspace: Worldspace,
     pub children: WorldChildren
@@ -10,27 +9,31 @@ pub struct WorldEntry {
 
 
 #[derive(Debug)]
-#[repr(C)]
 pub struct WorldChildren {
     pub header: GroupHeader,
     pub cell: Cell
 }
 
+
+// Implement nom_derive::Parse
 impl Parse<&[u8]> for WorldChildren {
     fn parse(i: &[u8]) -> IResult<&[u8], Self, nom::error::Error<&[u8]>> {
+
+        // Parse header and raw data pointer
         let (i, (header, raw)) = alloc_group(i)?;
 
-        
-
+        // Ensure correct group type - debugging only
         #[cfg(debug_assertions)]
         match header.label {
             GroupLabel::WorldChildren(_) => { }
             _ => { panic!("WorldChildren::parse encountered wrong group type: {:?}", header.label) }
         }
     
+        // Parse the Cell record inside the WorldChildren group
         let (raw, cell) = Cell::parse(raw)?;
 
-        let (_, next_id) = FourCC::parse(raw)?;
+
+        let (_, next_id) = GroupHeader::parse(raw)?;
         println!("Next ID after WorldChildren Cell: {:?}", next_id);
 
         println!("{:?}", cell);
