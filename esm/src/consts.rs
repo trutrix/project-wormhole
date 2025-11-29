19cb9f0e54673a04d3edf8872e99a1741e75e264
tRuTrIx <trutrix@gmail.com>
2025-11-29 10:23:08 -0700

    Added GRUP for checks

--- esm/src/consts.rs
+++ esm/src/consts.rs
@@ -5,3 +5,5 @@ pub const CELL_WIDTH_M: f32 = 58.5;
 pub const CELL_WIDTH_CM: f32 = CELL_WIDTH_M * 100.0;
 pub const CELL_WIDTH_MM: f32 = CELL_WIDTH_M * 1000.0;
 pub const UE_TO_CE_SCALE: f32 = 1.42822265625; // Unvalidated
+
+pub const GRUP: &[u8; 4] = b"GRUP";
