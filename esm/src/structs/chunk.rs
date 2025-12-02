91bda253fad52e83d31b1d1220f77d9d794167f7
tRuTrIx <trutrix@gmail.com>
2025-12-02 08:49:31 -0700

    Created smartchunks for future multithreading

--- esm/src/structs/chunk.rs
+++ esm/src/structs/chunk.rs
@@ -1,4 +1,4 @@
-use crate::{dev::*, structs::data};
+use crate::{dev::*, records::all::FileHeader, structs::data};
 
 
 
@@ -15,8 +15,6 @@ impl<'esm> Parse<&'esm[u8]> for ESMChunk<'esm> {
         Ok((i, chunk))
     }
 }
-
-
 
 pub fn alloc_chunk(i: &'_ [u8]) -> IResult<&'_ [u8], ESMChunk<'_>> {
     let orig = i;
@@ -38,4 +36,45 @@ pub fn alloc_chunk(i: &'_ [u8]) -> IResult<&'_ [u8], ESMChunk<'_>> {
 pub fn get_file_chunks(i: &'_ [u8]) -> IResult<&'_ [u8], Vec<ESMChunk<'_>>> {
     let (i, chunks) = many0(ESMChunk::parse)(i)?;
     Ok((i, chunks))
+}
+
+
+#[derive(Debug)]
+pub struct SmartChunks<'esm> {
+    pub header: FileHeader,
+    pub data_groups: Vec<ESMChunk<'esm>>,
+    pub reference_groups: Vec<ESMChunk<'esm>>
+}
+
+
+impl<'esm> Parse<&'esm[u8]> for SmartChunks<'esm> {
+    fn parse(i: &'esm[u8]) -> IResult<&'esm[u8], Self, nom::error::Error<&'esm[u8]>> {
+        let mut data_groups = Vec::new();
+        let mut reference_groups = Vec::new();
+        
+        
+        let (i, header) = FileHeader::parse(i)?;
+
+        let mut remaining = i;
+
+        while remaining.len() > 0 {
+
+            let (_, header) = GroupHeader::parse(remaining)?;
+            let (i, chunk) = alloc_chunk(remaining)?;
+            remaining = i;
+
+            if REFERENCE_GROUPS.contains(&&header.iden.0) {
+                reference_groups.push(chunk);
+            } else {
+                data_groups.push(chunk);
+            }
+
+
+        }
+
+
+        Ok((remaining, Self { header, data_groups, reference_groups }))
+
+
+    }
 }
