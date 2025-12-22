4530477e7befd8ef6c0b6bc754adcb4318a93cbd
tRuTrIx <trutrix@gmail.com>
2025-12-22 10:29:05 -0700

    Fixed use paths

--- shared/src/structs/sized_string.rs
+++ shared/src/structs/sized_string.rs
@@ -9,7 +9,7 @@ pub struct SizedString<T> {
 
 
 impl nom_derive::Parse<&[u8]> for SizedString<u32> {
-    fn parse(i: &[u8]) -> nom_derive::nom::IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
         let (i, size) = le_u32(i)?;
         let (i, value) = take(size)(i)?;
 
@@ -22,7 +22,7 @@ impl nom_derive::Parse<&[u8]> for SizedString<u32> {
 }
 
 impl nom_derive::Parse<&[u8]> for SizedString<u16> {
-    fn parse(i: &[u8]) -> nom_derive::nom::IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
         let (i, size) = le_u16(i)?;
         let (i, value) = take(size)(i)?;
 
@@ -35,7 +35,7 @@ impl nom_derive::Parse<&[u8]> for SizedString<u16> {
 }
 
 impl nom_derive::Parse<&[u8]> for SizedString<u8> {
-    fn parse(i: &[u8]) -> nom_derive::nom::IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
         let (i, size) = le_u8(i)?;
         let (i, value) = take(size)(i)?;
 
