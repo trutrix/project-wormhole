37200a9e929c1cc692126bf25575e26d895a3df0
tRuTrIx <trutrix@gmail.com>
2025-12-28 10:10:01 -0700

    Made fields public

--- esm/src/structs/colors.rs
+++ esm/src/structs/colors.rs
@@ -3,23 +3,23 @@ use crate::dev::*;
 
 #[derive(Debug, NomLE)]
 pub struct Color4 {
-    r: u8,
-    g: u8,
-    b: u8,
-    a: u8
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+    pub a: u8
 }
 
 
 #[derive(Debug, NomLE)]
 pub struct Color3 {
-    r: u8,
-    g: u8,
-    b: u8
+    pub r: u8,
+    pub g: u8,
+    pub b: u8
 }
 
 
 #[derive(Debug, NomLE)]
 pub struct Color2 {
-    r: u8,
-    g: u8
+    pub r: u8,
+    pub g: u8
 }
