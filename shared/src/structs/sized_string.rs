d9d05a168458c475fab68685305fe0549f1a76f4
tRuTrIx <trutrix@gmail.com>
2025-12-31 08:36:19 -0700

    Added better handled SizedStrings with clearer names

--- shared/src/structs/sized_string.rs
+++ shared/src/structs/sized_string.rs
@@ -1,6 +1,6 @@
 
 
-use nom_derive::nom::{IResult, number::complete::{le_u8, le_u16, le_u32}, bytes::complete::take};
+use nom_derive::{Parse, nom::{IResult, bytes::complete::take, number::complete::{le_u8, le_u16, le_u32}}};
 
 pub struct SizedString<T> {
     pub size: T,
@@ -46,3 +46,93 @@ impl nom_derive::Parse<&[u8]> for SizedString<u8> {
         }
     }
 }
+
+
+// ================================================================================
+
+
+pub struct U8ESMString(pub String);
+
+impl Parse<&[u8]> for U8ESMString {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
+        let (i, size) = le_u8(i)?;
+
+        if size == 0 {
+            return Ok((i, U8ESMString(String::new())));
+        }
+
+        // Discard the trailing null byte
+        let (i, value) = take(size-1)(i)?;
+
+        if let Ok(s) = String::from_utf8(value.to_vec()) {
+            Ok((i, U8ESMString(s)))
+        } else {
+
+            #[cfg(debug_assertions)]
+            panic!("Failed to parse U32ESMString: Invalid UTF-8 sequence");
+
+            Err(nom_derive::nom::Err::Error(nom_derive::nom::error::Error::new(i, nom_derive::nom::error::ErrorKind::MapRes)))
+        }
+    }
+}
+
+
+// ================================================================================
+
+pub struct U16ESMString(pub String);
+
+impl Parse<&[u8]> for U16ESMString {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
+        let (i, size) = le_u16(i)?;
+
+        if size == 0 {
+            return Ok((i, U16ESMString(String::new())));
+        }
+        
+        // Discard the trailing null byte
+        let (i, value) = take(size-1)(i)?;
+
+        if let Ok(s) = String::from_utf8(value.to_vec()) {
+            Ok((i, U16ESMString(s)))
+        } else {
+
+            #[cfg(debug_assertions)]
+            panic!("Failed to parse U32ESMString: Invalid UTF-8 sequence");
+
+            Err(nom_derive::nom::Err::Error(nom_derive::nom::error::Error::new(i, nom_derive::nom::error::ErrorKind::MapRes)))
+        }
+    }
+}
+
+
+// ================================================================================
+
+pub struct U32ESMString(pub String);
+
+impl Parse<&[u8]> for U32ESMString {
+    fn parse(i: &[u8]) -> IResult<&[u8], Self, nom_derive::nom::error::Error<&[u8]>> {
+        let (i, size) = le_u32(i)?;
+
+        if size == 0 {
+            return Ok((i, U32ESMString(String::new())));
+        }
+
+        // Discard the trailing null byte
+        let (i, value) = take(size-1)(i)?;
+
+        if let Ok(s) = String::from_utf8(value.to_vec()) {
+            Ok((i, U32ESMString(s)))
+        } else {
+
+            #[cfg(debug_assertions)]
+            panic!("Failed to parse U32ESMString: Invalid UTF-8 sequence");
+
+            Err(nom_derive::nom::Err::Error(nom_derive::nom::error::Error::new(i, nom_derive::nom::error::ErrorKind::MapRes)))
+        }
+    }
+}
+
+
+// ================================================================================
+
+
