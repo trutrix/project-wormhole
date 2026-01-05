cf0aead7b44181285af48d1d745aece918dd8083
tRuTrIx <trutrix@gmail.com>
2026-01-05 10:20:15 -0700

    Added note about error from UnrealEngine import

---
+++ nif/src/export/gltf.rs
@@ -0,0 +1,3 @@
+
+
+// Note from current UE 5.7 import: [deathclaw : '', Unknown] The same Joint(s) are used in multiple Skins with multiple different InverseBindMatrix values, which is not supported. Ignoring InverseBindMatrices for the entire Import. Offending Joints' Names: RFOOT.
