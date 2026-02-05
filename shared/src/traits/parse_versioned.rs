af63e8c4172a510a1871c64235fafc15ce99adf8
tRuTrIx <trutrix@gmail.com>
2026-02-05 08:43:55 -0700

    Made trait more flexible

--- shared/src/traits/parse_versioned.rs
+++ shared/src/traits/parse_versioned.rs
@@ -2,8 +2,6 @@
 use nom_derive::nom::IResult;
 
 
-pub trait ParseVersioned<T>: Sized {
-    fn parse_versioned(i: &[u8], version: T) -> IResult<&[u8], Self>;
-    #[allow(unused_variables)]
-    fn parse_versioned_depth(i: &[u8], version: T, depth: u8) -> IResult<&[u8], Self> { unimplemented!("You called this without actually implementing it.") }
+pub trait ParseVersioned<'nom, T, E>: Sized {
+    fn parse_versioned(i: &'nom[u8], version: T) -> IResult<&'nom[u8], Self, E>;
 }
