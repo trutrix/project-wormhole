89b178c000663b893582362d694bd53bc4472125
tRuTrIx <trutrix@gmail.com>
2026-03-02 08:42:52 -0700

    Change diff output to use comfy_table

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -88,6 +88,8 @@ fn main() {
 
         TopCommands::Diff { path_a, path_b, threads, report, report_path}=> {
             
+            let total_start = std::time::Instant::now();
+
             let path_a = std::path::Path::new(&path_a);
             let path_b = std::path::Path::new(&path_b);
 
@@ -105,18 +107,50 @@ fn main() {
                 panic!("Directories not supported yet");
             }
 
+            let read_start_a = std::time::Instant::now();
             let esm_file_a = std::fs::read(path_a).expect("Failed to read file A");
-            let esm_file_b = std::fs::read(path_b).expect("Failed to read file B");
+            let read_duration_a = read_start_a.elapsed();
 
+            let read_start_b = std::time::Instant::now();
+            let esm_file_b = std::fs::read(path_b).expect("Failed to read file B");
+            let read_duration_b = read_start_b.elapsed();
+
+            let parse_start_a = std::time::Instant::now();
             let (_, esm_a) = esm::diff::ESMDiff::parse(&esm_file_a).expect("Error parsing ESM file A.");
+            let parse_duration_a = parse_start_a.elapsed();
+
+            let parse_start_b = std::time::Instant::now();
             let (_, mut esm_b) = esm::diff::ESMDiff::parse(&esm_file_b).expect("Error parsing ESM file B.");
+            let parse_duration_b = parse_start_b.elapsed();
 
             let start = std::time::Instant::now();
             let result = esm::diff::get_diff_form_ids(&esm_a, &mut esm_b);
             let duration = start.elapsed();
+            let total_duration = total_start.elapsed();
 
-            result.print_summary();
-            println!("Diffing took: {:?}", duration);
+
+            // println!("File A read time: {:?}", read_duration_a);
+            // println!("File B read time: {:?}", read_duration_b);
+            // println!("File A parse time: {:?}", parse_duration_a);
+            // println!("File B parse time: {:?}", parse_duration_b);
+            // println!("Diffing took: {:?}", duration);
+            // println!("Total time: {:?}", total_duration);
+
+
+            let mut table = comfy_table::Table::new();
+            table.set_header(vec!["Change Type", "Count", "", "Benchmark Name", "Benchmark Time"]);
+            table.add_row(vec!["Header Changed", &result.header_changed.to_string(), "", "Parse A", &format!("{:?}", parse_duration_a)]);
+            table.add_row(vec!["Additions", &result.additions.len().to_string(), "", "Parse B", &format!("{:?}", parse_duration_b)]);
+            table.add_row(vec!["Deletions", &result.deletions.len().to_string(), "", "File Read A", &format!("{:?}", read_duration_a)]);
+            table.add_row(vec!["Changed", &result.changed.len().to_string(), "", "File Read B", &format!("{:?}", read_duration_b)]);
+            table.add_row(vec!["Same", &result.same.len().to_string(), "", "Diffing", &format!("{:?}", duration)]);
+            table.add_row(vec!["", "", "", "Total Time", &format!("{:?}", total_duration)]);
+
+
+            println!("");
+            println!("{}", table);
+            println!("");
+
         }
     }
     
