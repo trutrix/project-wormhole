25dc385ddd4845b868ee7a709310c1d6d1f5f8a9
tRuTrIx <trutrix@gmail.com>
2026-04-08 08:22:46 -0600

    Changed raw exterior cell block to a type alias

--- esm/src/groups/exterior_cell_block.rs
+++ esm/src/groups/exterior_cell_block.rs
@@ -41,48 +41,42 @@ impl Parse<&[u8]> for ExteriorCellBlock {
 
 impl<'esm> MapContents<HashMap<FormId, RawRecord<'esm>>> for RawExteriorCellBlock<'esm> {
     fn insert_into_one_map(self, map: &mut HashMap<FormId, RawRecord<'esm>>) {
-        for sub_block in self.sub_blocks {
-            for cell in sub_block.cells {
+        for sub_block in self.data {
+            for cell in sub_block.data {
                 cell.insert_into_one_map(map);
             }
         }
     }
-
-    fn insert_into_two_maps(self, map1: &mut HashMap<FormId, RawRecord<'esm>>, map2: &mut HashMap<FormId, RawRecord<'esm>>) {
-        for sub_block in self.sub_blocks {
-            for cell in sub_block.cells {
-                cell.insert_into_two_maps(map1, map2);
-            }
-        }
-    }
 }
 
 // ====================================================================================================
 
-#[derive(Debug)]
-pub struct RawExteriorCellBlock<'esm> {
-    pub header: GroupHeader,
-    pub sub_blocks: Vec<RawExteriorCellSubBlock<'esm>>
-}
+// #[derive(Debug)]
+// pub struct RawExteriorCellBlock<'esm> {
+//     pub header: GroupHeader,
+//     pub sub_blocks: Vec<RawExteriorCellSubBlock<'esm>>
+// }
 
-// ====================================================================================================
+// // ====================================================================================================
 
-impl<'esm> Parse<&'esm[u8]> for RawExteriorCellBlock<'esm> {
-    fn parse(i: &'esm[u8]) -> IResult<&'esm[u8], Self, nom::error::Error<&'esm[u8]>> {
-        let (i, (header, raw)) = alloc_group(i)?;
+// impl<'esm> Parse<&'esm[u8]> for RawExteriorCellBlock<'esm> {
+//     fn parse(i: &'esm[u8]) -> IResult<&'esm[u8], Self, nom::error::Error<&'esm[u8]>> {
+//         let (i, (header, raw)) = alloc_group(i)?;
 
-        if let GroupLabel::ExteriorCellBlock(_block_coords) = header.label {
+//         if let GroupLabel::ExteriorCellBlock(_block_coords) = header.label {
 
-            let (raw, sub_blocks) = many0(RawExteriorCellSubBlock::parse)(raw)?;
+//             let (raw, sub_blocks) = many0(RawExteriorCellSubBlock::parse)(raw)?;
 
-            #[cfg(debug_assertions)]
-            if !raw.is_empty() {
-                panic!("Failed to consume RawExteriorBlock");
-            }
+//             #[cfg(debug_assertions)]
+//             if !raw.is_empty() {
+//                 panic!("Failed to consume RawExteriorBlock");
+//             }
 
-            Ok((i, Self { header, sub_blocks }))
-        } else {
-            panic!("RawExteriorCellBlock::parse encountered wrong group type: {:?}", header.label)
-        }        
-    }
-}
+//             Ok((i, Self { header, sub_blocks }))
+//         } else {
+//             panic!("RawExteriorCellBlock::parse encountered wrong group type: {:?}", header.label)
+//         }        
+//     }
+// }
+
+pub type RawExteriorCellBlock<'esm> = Group<Group<RawCellRecord<'esm>>>;
