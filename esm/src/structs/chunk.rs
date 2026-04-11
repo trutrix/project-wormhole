a5d19ae7898d61c6dcfa89c8cccb5ed7c237cc3f
tRuTrIx <trutrix@gmail.com>
2026-04-11 10:26:55 -0600

    Cleaned up, fixed lifetimes, added comments

--- esm/src/structs/chunk.rs
+++ esm/src/structs/chunk.rs
@@ -1,13 +1,14 @@
-use crate::{dev::*, records::all::FileHeader};
-
+use crate::dev::*;
 
 // ====================================================================================================
 
+/// Top level data pointer
 #[derive(Debug)]
 pub struct ESMChunk<'esm> {
     pub data: &'esm[u8]
 }
 
+// ====================================================================================================
 
 impl<'esm> Parse<&'esm[u8]> for ESMChunk<'esm> {
     fn parse(i: &'esm[u8]) -> IResult<&'esm[u8], Self, nom::error::Error<&'esm[u8]>> {
@@ -16,26 +17,45 @@ impl<'esm> Parse<&'esm[u8]> for ESMChunk<'esm> {
     }
 }
 
-pub fn alloc_chunk(i: &'_ [u8]) -> IResult<&'_ [u8], ESMChunk<'_>> {
+// ====================================================================================================
+
+pub fn alloc_chunk<'esm>(i: &'esm [u8]) -> IResult<&'esm [u8], ESMChunk<'esm>> {
+    // Keep original pointer
     let orig = i;
+
+    // Parse the iden
     let (i, iden) = FourCC::parse(i)?;
+
+    // Parse the size
     let (_, size) = u32::parse_le(i)?;
 
+    // If size zero, return empty buffer
     if size == 0 {
         Ok((i, ESMChunk { data: &[] }))
-    } else if &iden.0 == b"TES4" {
+    }
+
+    // If iden is header, add 24 to size of data buffer
+    else if &iden.0 == b"TES4" {
         let (i, data) = take(size + 24)(orig)?;
         Ok((i, ESMChunk { data }))
-    } else if &iden.0 == b"GRUP" {
+    } 
+
+    // If the iden is a group just take size normally
+    else if &iden.0 == b"GRUP" {
+
         let (i, data) = take(size)(orig)?;
         Ok((i, ESMChunk { data }))
-    } else {
+    } 
+    
+    // Undefined behavior
+    else {
         panic!("alloc_chunk encountered unexpected chunk type: {:?}", iden);
     }
 }
 
+// ====================================================================================================
 
-pub fn get_file_chunks(i: &'_ [u8]) -> IResult<&'_ [u8], Vec<ESMChunk<'_>>> {
+pub fn get_file_chunks<'esm>(i: &'esm [u8]) -> IResult<&'esm [u8], Vec<ESMChunk<'esm>>> {
     let (i, chunks) = many0(ESMChunk::parse)(i)?;
     Ok((i, chunks))
 }
